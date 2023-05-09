b9dfa7c5b9e8a34458e5f5e79a69a7c9357400ef
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2023-05-09 20:45:50 +0300

    logout

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 from .forms import LoginForm
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
+    #path("accounts/", include("django.contrib.auth.urls")),
 ]
 
--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ from item.models import Category, Item
 
 from .forms import SignupForm
 
+from django.contrib.auth import logout
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
