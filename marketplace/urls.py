c8248d1870d5fa80ae245f06ea05839caf7f126e
Lawrence Kyalo <lawrencekkyalo@gmail.com>
2023-05-16 12:37:37 +0300

    removing redundant logout links in urls

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('items/', include('item.urls')),
     path('dashboard', include('dashboard.urls')),
     path('inbox', include('conversation.urls')),
-    #path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
